838b665a4a2fa2826756bb6ab5eac21f95e606be
SETARI1 <155354272+SETARI1@users.noreply.github.com>
2024-03-22 10:37:10 +0800

    Add files via upload

---
+++ installer.py
@@ -0,0 +1,42 @@
+import os
+print ("""
+                                             
+                             \033[95m⣿⣿⣿⠿⠿⣿⣿⡿⢋⣶⣶⣬⣙⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+                             \033[95⣿⡿⢡⣿⣷⣶⣦⣥⣿⣿⣿⣿⣿⣷⣮⡛⢿⣿⣿⣿⣿⣿⣿⣿
+                             \033[95⣿⡇⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⢮⡙⣿⣿⣯⢐⡎⣿
+                             \033[95⣿⢹⣿⣿⣿⣿⣿⣿⡿⣡⡬⢿⣿⣿⣿⣶⣶⣼⣦⠥⣖⣩⣾⣿
+                             \033[95⣿⢸⣿⣿⣿⡿⣿⣿⣿⣿⠇⣌⢛⣻⣿⣿⣟⣛⣿⣧⠹⣿⣿⣿
+                             \033[95⠏⣼⣿⣿⢏⣾⣿⣟⣩⣶⣶⣿⣿⣿⣿⣿⡟⡿⢸⡿⣡⣿⣿⣿
+                             \033[95⣼⣿⣿⠇⣼⣿⣿⢸⠋⠁⠉⢽⣿⣿⣿⣟⣠⣤⣆⢃⢻⣿⣿⣿
+                             \033[95⣿⣿⣿⣼⣿⣿⣿⡞⣿⣿⣷⣾⣿⣿⣿⣿⡿⠟⠛⠸⢦⣙⡋⣿
+                             \033[95⣿⣿⣿⠹⣿⣿⡿⠗⣈⣭⣭⣭⣉⠻⡟⣩⣶⣾⣿⣿⣶⡙⣱⣿
+                             \033[95⣿⣿⣿⣷⣌⡛⠠⣿⣿⣿⣿⣿⣿⣿⣾⣿⣿⣿⣿⣿⣿⣿⢸⣿
+                             \033[95⣿⣿⣿⣿⢏⣴⣧⣴⡘⣿⣿⣿⣿⣿⣿⣿⣿⣿⣱⣶⣴⡜⢸⣿
+                             \033[95⣿⣿⣿⢃⣾⣿⣿⣿⡷⠉⢿⣿⣿⣿⣿⣿⣿⢰⣾⣿⣿⣧⢸⣿
+                          
+""")
+print("""\33[0;32m[1] RUN\n[2] CANCEL\nCancel or Run?""")
+c = input(">>>: ")
+if c == "1":
+    os.system("npm install gradient-string")
+    os.system("npm install path")
+    os.system("npm install net")
+    os.system("npm install http2")
+    os.system("npm install tls")
+    os.system("npm install url")
+    os.system("npm install crypto")
+    os.system("npm install fs")
+    os.system("npm install colors")
+    os.system("npm install os")
+    os.system("npm install http")
+    os.system("npm install fake-useragent")
+    os.system("npm install axios")
+    os.system("npm install useragent")
+    os.system("npm install minimist")
+    os.system("npm install hex")
+    os.system("npm install events")
+    os.system("npm install cluster")
+    
+elif c == "2":
+    os.system("clear")
+print ("DONE!")
